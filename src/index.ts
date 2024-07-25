22d5c547922f40b5f50192c5b24684bdd65f25d4
dineshraj <dineshraj.goomany@gmail.com>
2024-07-25 15:42:30 +0100

    initial commit to get input via the command line and adding types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
-// NOTE: we have only provided this as a skeleton & you are free remove it if you want.
-export function runWith(_input: any) {
-  return { status: 'error' };
+import { readFileSync } from 'fs';
+
+type heading = 'north' | 'south' | 'east' | 'west';
+type direction = 'forward' | 'left' | 'right';
+
+const validDirections: Array<direction> = ['forward', 'left', 'right'];
+
+interface Arena {
+  corner1: {
+    x: number;
+    y: number;
+  };
+  corner2: {
+    x: number;
+    y: number;
+  };
 }
 
-runWith(undefined);
+interface Input {
+  arena: Arena;
+  location: {
+    x: number;
+    y: number;
+  };
+  heading: heading;
+  directions: Array<direction>;
+}
+
+const runWith = (input : Input) => {
+  console.log('🚀 ~ runWith ~ input:', input);
+  return { status: 'error' };
+};
+
+if (process.env.NODE_ENV === 'production') {
+  try {
+    const json = JSON.parse(readFileSync(process.stdin.fd, 'utf8'));
+    runWith(json);
+  } catch (e) {
+    console.error('Error reading from stdin', e);
+  }
+}
+
+export { runWith };
